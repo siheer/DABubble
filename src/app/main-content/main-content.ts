9feb4b3d3e1caf3e3aef7843021885086525ff31
Dave Hannemann <davidhannemanndev@gmail.com>
2025-11-30 16:26:50 +0100

    feat: added some pictures and started navbar

--- src/app/main-content/main-content.ts
+++ src/app/main-content/main-content.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { Workspace } from './workspace/workspace';
 import { Navbar } from './navbar/navbar';
+import { Messages } from './messages/messages';
+import { Thread } from './thread/thread';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [MatSidenavModule, Workspace, Navbar],
+  imports: [MatSidenavModule, Workspace, Navbar, Messages, Thread],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
-export class MainContent {
-
-}
+export class MainContent {}
