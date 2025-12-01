333a14b6d27cf45276013d85f069a256e98b05fa
Robin Erike <robinerike@gmail.com>
2025-12-01 10:46:05 +0100

    channellist in workspace

--- src/app/main-content/workspace/workspace.ts
+++ src/app/main-content/workspace/workspace.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable, map } from 'rxjs';
+
+type Channel = { id: string; title?: string };
+
 
 @Component({
   selector: 'app-workspace',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './workspace.html',
   styleUrl: './workspace.scss',
 })
 export class Workspace {
+  private readonly firestore = inject(Firestore);
+  protected readonly channels$: Observable<Channel[]> = this.loadChannels();
 
+  private loadChannels(): Observable<Channel[]> {
+    const channelsLocation = collection(this.firestore, 'channels');
+    return collectionData(channelsLocation, { idField: 'id' }).pipe(map((channels) => channels as Channel[])
+    );
+
+  }
 }
