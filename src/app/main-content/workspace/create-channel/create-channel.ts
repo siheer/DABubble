70d4d4e7303b8e5de8791fc76d5d5e043e721908
Robin Erike <robinerike@gmail.com>
2025-12-01 11:29:11 +0100

    new component createchannel

---
+++ src/app/main-content/workspace/create-channel/create-channel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-channel',
+  imports: [],
+  templateUrl: './create-channel.html',
+  styleUrl: './create-channel.scss',
+})
+export class CreateChannel {
+
+}
