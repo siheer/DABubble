e6b73e077e9737d0b7fd176ec070337ae89fda5d
Robin Erike <robinerike@gmail.com>
2025-12-03 11:04:58 +0100

    open and close the channels and userslist comes next

--- src/app/main-content/workspace/workspace.ts
+++ src/app/main-content/workspace/workspace.ts
@@ -5,8 +5,7 @@ import { Observable, map } from 'rxjs';
 import { CreateChannel } from './create-channel/create-channel';
 
 type Channel = { id: string; title?: string };
-type MenuEntry = { label: string };
-type DirectMessage = { name: string; status: 'online' | 'offline' };
+type DirectMessage = { name: string;};
 
 @Component({
   selector: 'app-workspace',
@@ -18,6 +17,7 @@ type DirectMessage = { name: string; status: 'online' | 'offline' };
 export class Workspace {
   private readonly firestore = inject(Firestore);
   protected readonly channels$: Observable<Channel[]> = this.loadChannels();
+  protected readonly directMessages$: Observable<DirectMessage[]> = this.loadDirectMessages();
 
   protected isCreateChannelOpen = false;
   protected openCreateChannel(): void {
@@ -30,6 +30,20 @@ export class Workspace {
   private loadChannels(): Observable<Channel[]> {
     const channelsLocation = collection(this.firestore, 'channels');
     return collectionData(channelsLocation, { idField: 'id' }).pipe(map((channels) => channels as Channel[])
+    );
+  }
+
+  private loadDirectMessages(): Observable<DirectMessage[]> {
+    let usersLocation = collection(this.firestore, 'users');
+
+    return collectionData(usersLocation, { idField: 'id' }).pipe(
+      map((users) =>
+        (users as Array<{ name?: string; }>).map(
+          (user) => ({
+            name: user.name ?? 'Unbenannter Nutzer',
+          })
+        )
+      )
     );
   }
 }
