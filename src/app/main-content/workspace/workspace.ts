3112f06feb7d8328eb9da4159418065e16d28d38
Robin Erike <robinerike@gmail.com>
2025-12-07 11:05:59 +0100

    Update workspace.ts

--- src/app/main-content/workspace/workspace.ts
+++ src/app/main-content/workspace/workspace.ts
@@ -6,6 +6,7 @@ import { CreateChannel } from './create-channel/create-channel';
 import { Channel, FirestoreService } from '../../services/firestore.service';
 import { AuthService } from '../../services/auth.service';
 import { User } from '@angular/fire/auth';
+import { ChannelSelectionService } from '../../services/channel-selection.service';
 
 @Component({
   selector: 'app-workspace',
@@ -17,8 +18,11 @@ import { User } from '@angular/fire/auth';
 export class Workspace {
   private readonly firestoreService = inject(FirestoreService);
   private readonly authService = inject(AuthService);
+    private readonly channelSelectionService = inject(ChannelSelectionService);
   protected readonly channels$: Observable<Channel[]> = this.firestoreService.getChannels();
   protected readonly currentUser$: Observable<User | null> = this.authService.user$;
+    protected readonly selectedChannelId$ =
+    this.channelSelectionService.selectedChannelId$;
   protected areChannelsCollapsed = false;
   protected areDirectMessagesCollapsed = false;
   protected isCreateChannelOpen = false;
@@ -30,6 +34,10 @@ export class Workspace {
   }
   protected toggleChannels(): void {
     this.areChannelsCollapsed = !this.areChannelsCollapsed;
+  }
+
+    protected selectChannel(channelId?: string | null): void {
+    this.channelSelectionService.selectChannel(channelId);
   }
 
   protected toggleDirectMessages(): void {
