97b811768c28e8997ecb5a7898d46163afa3c2d4
Robin Erike <robinerike@gmail.com>
2025-12-10 11:21:00 +0100

    error

--- src/app/auth/functions/src/index.ts
+++ src/app/auth/functions/src/index.ts
@@ -4,25 +4,29 @@ import * as functions from "firebase-functions";
 admin.initializeApp();
 const db = admin.firestore();
 
-export const onAuthUserCreate = functions.auth.user().onCreate(async (user) => {
-  const ref = db.doc(`users/${user.uid}`);
-  const doc = await ref.get();
-  if (doc.exists) return;
+export const onAuthUserCreate = functions.auth
+    .user()
+    .onCreate(async (user: admin.auth.UserRecord) => {
+        const ref = db.doc(`users/${user.uid}`);
+        const doc = await ref.get();
+        if (doc.exists) return;
 
-  await ref.set({
-    uid: user.uid,
-    email: user.email ?? null,
-    name: user.displayName || user.email || "Gast",
-    photoUrl: user.photoURL || "",
-    onlineStatus: false,
-    emailVerified: user.emailVerified ?? false,
-    providerIds: user.providerData.map((p) => p.providerId),
-    createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    updatedAt: admin.firestore.FieldValue.serverTimestamp(),
-    lastSeen: admin.firestore.FieldValue.serverTimestamp(),
-  });
-});
+        await ref.set({
+            uid: user.uid,
+            email: user.email ?? null,
+            name: user.displayName || user.email || "Gast",
+            photoUrl: user.photoURL || "",
+            onlineStatus: false,
+            emailVerified: user.emailVerified ?? false,
+            providerIds: user.providerData.map((p: admin.auth.UserInfo) => p.providerId),
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+            updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+            lastSeen: admin.firestore.FieldValue.serverTimestamp(),
+        });
+    });
 
-export const onAuthUserDelete = functions.auth.user().onDelete(async (user) => {
-  await db.doc(`users/${user.uid}`).delete().catch(() => {});
-});
+export const onAuthUserDelete = functions.auth
+    .user()
+    .onDelete(async (user: admin.auth.UserRecord) => {
+        await db.doc(`users/${user.uid}`).delete().catch(() => { });
+    });
