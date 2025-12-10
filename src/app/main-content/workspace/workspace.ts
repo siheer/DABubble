52cb8985f4479ba5c1ba7501fe50efc276b797b7
Robin Erike <robinerike@gmail.com>
2025-12-10 11:17:05 +0100

    admin error

--- src/app/main-content/workspace/workspace.ts
+++ src/app/main-content/workspace/workspace.ts
@@ -1,14 +1,13 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, combineLatest, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { CreateChannel } from './create-channel/create-channel';
 
 import {
   Channel,
   DirectMessage,
   FirestoreService,
-} from '../../services/firestore.service'; import { AuthService } from '../../services/auth.service';
-import { User } from '@angular/fire/auth';
+} from '../../services/firestore.service';
 import { ChannelSelectionService } from '../../services/channel-selection.service';
 
 @Component({
@@ -20,20 +19,12 @@ import { ChannelSelectionService } from '../../services/channel-selection.servic
 })
 export class Workspace {
   private readonly firestoreService = inject(FirestoreService);
-  private readonly authService = inject(AuthService);
   private readonly channelSelectionService = inject(ChannelSelectionService);
   @Output() readonly newMessage = new EventEmitter<void>();
   protected readonly channels$: Observable<Channel[]> =
     this.firestoreService.getChannels();
-  protected readonly directMessages$: Observable<DirectMessage[]> = combineLatest([
-    this.firestoreService.getDirectMessages(),
-    this.authService.user$,
-  ]).pipe(
-    map(([directMessages, currentUser]) =>
-      directMessages.filter((directMessage) => directMessage.id !== currentUser?.uid)
-    )
-  );
-  protected readonly currentUser$: Observable<User | null> = this.authService.user$;
+  protected readonly directMessages$: Observable<DirectMessage[]> =
+    this.firestoreService.getDirectMessages();
   protected readonly selectedChannelId$ =
     this.channelSelectionService.selectedChannelId$;
   protected areChannelsCollapsed = false;
