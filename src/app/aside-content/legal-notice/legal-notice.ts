0f5752aca251f328ad53363097a6568a8ba17085
siheer <182635383+siheer@users.noreply.github.com>
2025-12-22 13:33:24 +0100

    feat (legal): add mockup for legal-pages

--- src/app/aside-content/legal-notice/legal-notice.ts
+++ src/app/aside-content/legal-notice/legal-notice.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { AsideContentWrapperComponent } from '../aside-content-wrapper';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [AsideContentWrapperComponent],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
 export class LegalNotice {
-
+  backInHistory() {
+    window.history.back();
+  }
 }
--- src/app/aside-content/privacy-policy/privacy-policy.ts
+++ src/app/aside-content/privacy-policy/privacy-policy.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { AsideContentWrapperComponent } from '../aside-content-wrapper';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [AsideContentWrapperComponent],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
 export class PrivacyPolicy {
-
+  backInHistory() {
+    window.history.back();
+  }
 }
